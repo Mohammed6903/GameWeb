767e86ea973705159b48c8ac3d80cabe03e1ecf4
Mohammed <mohammedusmani2005@gmail.com>
2024-12-29 14:46:18 +0530

    moved robots.txt and sitemap configured

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    siteUrl: process.env.NEXT_PUBLIC_SITE_URL, // Replace with your domain
+    // generateRobotsTxt: true,
+    changefreq: "daily",
+    priority: 0.7,
+    sitemapSize: 5000,
+};  
